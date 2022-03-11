5f2b9bb19988256e5551e870d81c1723e51a9606
Krilliam <79572252+Krilliam@users.noreply.github.com>
2022-03-11 10:33:14 +0100

    First commit

---
+++ donPerignonsQuest/cmake-build-debug/CMakeFiles/donPerignonsQuest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for donPerignonsQuest.
+# This may be replaced when dependencies are built.
---
+++ donPerignonsQuest/cmake-build-debug/CMakeFiles/tentativoSFML.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tentativoSFML.dir/main.cpp.obj"
+  "CMakeFiles/tentativoSFML.dir/main.cpp.obj.d"
+  "libtentativoSFML.dll.a"
+  "tentativoSFML.exe"
+  "tentativoSFML.exe.manifest"
+  "tentativoSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tentativoSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
